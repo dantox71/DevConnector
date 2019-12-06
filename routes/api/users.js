d8bf3e213ec3ace4c2e27b5fb28a2b7c0c269911
Daniel <Daniellagowskii@gmail.com>
2019-12-06 17:06:25 +0100

    add functionality to route that is responsible for registering a new user.Now when someone hit a /api/users route, and entered data is valid, user will be added to the database

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+//User model
+const User = require("../../models/User");
 
 // @route   POST api/users
 // @desc    Register a user
@@ -21,7 +26,7 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
 
     //Check for errors
@@ -29,8 +34,48 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    console.log(req.body);
-    res.send(req.body);
+    const { name, email, password } = req.body;
+
+    try {
+      //See if user exists
+      let user = await User.findOne({ email: email });
+
+      //User  with this email already exists
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      //Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      //Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      //Save user to the database
+      await user.save();
+
+      res.send(user);
+
+      //Return jsonwebtoken
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
