9da2cb7eff35a984ac065b3a16e5e51667eeee42
Daniel <Daniellagowskii@gmail.com>
2019-12-07 12:50:57 +0100

    Add DELETE /api/profile that will delete logged in user and his profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -126,7 +126,6 @@ router.get("/", async (req, res) => {
 
     res.json(profiles);
   } catch (err) {
-    console.error(err.message);
     res.status(500).send("Server error");
   }
 });
@@ -152,6 +151,25 @@ router.get("/user/:user_id", async (req, res) => {
     }
 
     res.status(500).send("Server error");
+  }
+});
+
+// @route   DELETE api/profile
+// @desc    Delete profile,user & posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    //@todo - remove user's posts
+
+    //Remove Profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.send(`User with id ${req.user.id} has been deleted(and his profile)`);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 });
 
