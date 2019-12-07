32728c048a7b596dfb29f0ee85797d3ec51dd1d6
Daniel <Daniellagowskii@gmail.com>
2019-12-07 10:01:17 +0100

    Added POST api/profile route that is responsible for creating or updateing user's profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
 // @route   GET api/profile/me
-// @desc    Get current users profile
+// @desc    Get current user's profile
 // @access  Private
 router.get("/me", auth, async (req, res) => {
   try {
@@ -19,11 +20,101 @@ router.get("/me", auth, async (req, res) => {
       return res.status(400).json({ msg: "There is no profile for that user" });
     }
 
+    //Send profile back
     res.json(profile);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
   }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user's profile
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required")
+        .not()
+        .isEmpty(),
+      check("skills", "Skills is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    //Check for errors
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    //Excract profile data from req.body
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
+
+    //Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
+
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (instagram) profileFields.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      //Check if authenticated user has already account. if so - that means that he wants to edit his profile.In other case that means that he wants to create a new one.
+      if (profile) {
+        //Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      } else {
+        //Create new profile
+        profile = new Profile(profileFields);
+
+        //Save to the database
+        await profile.save();
+
+        res.json(profile);
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
