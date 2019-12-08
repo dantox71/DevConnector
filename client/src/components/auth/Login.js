a7d2d8b23ae65675158e0e971e52065196625ab6
Daniel <Daniellagowskii@gmail.com>
2019-12-08 20:04:22 +0100

    Add Register & Login Components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = async e => {
+    if (password === "" || email === "") {
+      console.log("Fill in all required fields");
+    } else {
+      console.log("Logged in");
+    }
+
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+
+      <p className="lead">
+        <i className="fa fa-user"></i> Sign Into Your Account
+      </p>
+
+      <form action="dashboard.html" className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
   );
 };
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,97 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = async e => {
+    if (password !== password2) {
+      console.log("Password do not match");
+    } else {
+      console.log("Registered");
+    }
+
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+
+      <p className="lead">
+        <i className="fa fa-user"></i> Create Your Account
+      </p>
+
+      <form action="dashboard.html" className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={name}
+            onChange={onChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={onChange}
+            required
+          />
+          <small className="form-text">
+            This site uses Gravatar, so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="password"
+            name="password2"
+            placeholder="Confirm Password"
+            value={password2}
+            onChange={onChange}
+            required
+          />
+        </div>
+
+        <input type="submit" value="Register" className="btn btn-primary" />
+      </form>
+
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </Fragment>
   );
 };
 
