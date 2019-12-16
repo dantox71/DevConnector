8adb301fd9910744ba9966fdc17ac628810e4ebb
Daniel <Daniellagowskii@gmail.com>
2019-12-16 19:36:21 +0100

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 //Connect Database
 connectDB();
-
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
 
 //Init Middleware
 app.use(express.json({ extended: false }));
@@ -18,6 +15,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/buiild", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
